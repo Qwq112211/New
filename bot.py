ccbefc351126d2e7a1a209bc17a920cfac0664fa
Qwq112211 <153113757+Qwq112211@users.noreply.github.com>
2023-12-06 23:30:45 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,4 @@
-
 from telethon.sync import TelegramClient
-from telethon.tl.functions.messages import ForwardMessagesRequest
-from telethon.tl.functions.channels import GetParticipantsRequest
-from telethon.tl.types import InputChannel, ChannelParticipantsSearch
-from telethon.tl.types import UserStatusRecently
-import re
 
 # Ваши данные API
 api_id = '22928444'
@@ -12,33 +6,20 @@ api_hash = '972e1a6e4116dbb7964a0f1ed9dc39ed'
 phone = '+6283132117844'
 session_file = '82K2'
 
-# Номер канала, откуда нужно брать текст
-source_channel = '-4045914293'
-# Номер канала, куда нужно отправлять текст
-destination_channel = '-4043745865'
+source_channel_username = 'source_channel_username'
+destination_channel_username = 'destination_channel_username'
 
-# Инициализация клиента
-client = TelegramClient(session_file, api_id, api_hash)
+with TelegramClient(session_file, api_id, api_hash) as client:
+    client.send_message(destination_channel_username, "Бот запущен")
 
-async def main():
-    await client.start(phone)
-    source = await client.get_entity(source_channel)
-    destination = await client.get_entity(destination_channel)
-
-    # Получение последних сообщений из первого канала
-    messages = await client.get_messages(source, limit=10)
-
-    for message in messages:
+    async for message in client.iter_messages(source_channel_username, limit=10):
         # Обработка текста (удаление ненужной информации)
-        processed_text = re.sub('H.U.G.O PROJECT', '', message.text)
-        
-        # Пересылка обработанного текста во второй канал
-        await client(ForwardMessagesRequest(
-            from_peer=source,
-            id=[message.id],
-            to_peer=destination
-        ))
+        processed_text = process_message(message.text)
 
-if __name__ == '__main__':
-    with client:
-        client.loop.run_until_complete(main())
+        # Отправка обработанного текста во второй канал
+        await client.send_message(destination_channel_username, processed_text)
+
+def process_message(text):
+    # Здесь вы можете добавить логику для обработки текста
+    # Например, удалить ненужные фрагменты, заменить слова и т.д.
+    return text
